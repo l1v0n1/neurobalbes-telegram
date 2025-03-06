b5f7f584ad2174323d981c9aeb8cf41880ca3b8e
anonim <70073044+l1v0n1@users.noreply.github.com>
2025-03-06 14:18:30 +0300

    edited

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,23 @@
+# Bot settings
 API_TOKEN = '123:123-123'
 admin = 123
 channel = 'https://t.me/123'
 channel_name = '@123'
 add_to_chat_link = 'http://t.me/123?startgroup=start'
 chat = 'https://t.me/123_chat'
+bot_username = '@123_bot'
+
+# Donate settings
 donate = 'https://www.donationalerts.com/r/spasibo_za_donati'
 donatetoken = '123'
+
+# Payment settings
 premiumamount = 149
 payoksecret = '123'
 payokapi = '123-123-123'
 payokapiid = '123'
 payokshopid= 3222
-bot_username = '@123_bot'
+
+# Version and github url
 version = '2.0.1'
 github_url = 'https://github.com/l1v0n1/neurobalbes-telegram'
